9af1dbae7092bb0c27c7734d9fb26853b42fa73e
Kik02 <Golovin.Savelii@yandex.ru>
2026-04-30 10:10:37 +0500

    Разработанные модели. bartova

---
+++ Models/Clubs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pr29_bartova.Models
+{
+    public class Clubs
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string WorkTime { get; set; }
+
+    }
+}
