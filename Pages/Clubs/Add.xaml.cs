5b9d0dd4855d703bf4569f38314b74028bda0a20
ADMIN <ADMIN@MSI>
2026-05-05 01:13:54 +0500

    Разработанный модуль управления клубами. Бартова

---
+++ Pages/Clubs/Add.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows.Controls;
+
+namespace pr29_bartova.Pages.Clubs
+{
+    public partial class Add : Page
+    {
+        Main Main;
+        Models.Clubs Club;
+
+        public Add(Main Main, Models.Clubs Club = null)
+        {
+            InitializeComponent();
+            this.Main = Main;
+            if (Club != null)
+            {
+                this.Club = Club;
+                this.Name.Text = Club.Name;
+                this.Address.Text = Club.Address;
+                this.WorkTime.Text = Club.WorkTime;
+                BthAdd.Content = "Изменить";
+            }
+        }
+        private void AddClub(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (this.Club == null)
+            {
+                Club = new Models.Clubs();
+                Club.Name = this.Name.Text;
+                Club.Address = this.Address.Text;
+                Club.WorkTime = this.WorkTime.Text;
+                this.Main.AllClub.Clubs.Add(this.Club);
+            }
+            else
+            {
+                Club.Name = this.Name.Text;
+                Club.Address = this.Address.Text;
+                Club.WorkTime = this.WorkTime.Text;
+            }
+            this.Main.AllClub.SaveChanges();
+            MainWindow.init.OpenPages(new Pages.Clubs.Main());
+        }
+    }
+}
---
+++ Pages/Clubs/Main.xaml.cs
@@ -0,0 +1,23 @@
+﻿using pr29_bartova.Classes;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace pr29_bartova.Pages.Clubs
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public ClubsContext AllClub = new ClubsContext();
+        public Main()
+        {
+            InitializeComponent();
+            foreach (Models.Clubs Club in AllClub.Clubs)
+                Parent.Children.Add(new Elements.Item(Club, this));
+        }
+
+        private void AddClub(object sender, RoutedEventArgs e)=>
+            MainWindow.init.OpenPages(new Pages.Clubs.Add(this));
+    }
+}
