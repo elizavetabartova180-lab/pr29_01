f939595f70d1be1e658ec6373402cbf28a7cdd82
ADMIN <ADMIN@MSI>
2026-05-07 09:05:37 +0500

    Сданная работа

--- Classes/Common/Config.cs
+++ Classes/Common/Config.cs
@@ -7,7 +7,7 @@ namespace pr29_bartova.Classes.Common
 {
     public class Config
     {
-        public static string ConnectionConfig = "server=; uid=root; pwd=; database=pcClub;";
+        public static string ConnectionConfig = "server=127.0.0.1; uid=root; pwd=; database=pcClub;";
         public static MySqlServerVersion Version = new MySqlServerVersion(new Version(8, 0, 11));
     }
 }
